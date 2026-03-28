66b22c331c715c3bf5de5b2d278c64009b1bd9dd
17adarshdash-commits <17adarshdash@gmail.com>
2026-03-28 22:48:38 +0530

    chore: Auto-generated EC2 downgrades via FinOps ML

---
+++ infrastructure/rightsizing_finops-rightsize-898b54.tf
@@ -0,0 +1,29 @@
+locals {
+  auto_downgrade_instances = {
+    "i-0540667a" = "t3.large" # Saving $75/mo
+    "i-0122092a" = "t3.large" # Saving $75/mo
+    "i-0599280a" = "t3.large" # Saving $75/mo
+    "i-0257519a" = "t3.large" # Saving $75/mo
+    "i-0127487a" = "t3.large" # Saving $75/mo
+    "i-0689722a" = "t3.large" # Saving $75/mo
+    "i-0292888a" = "t3.large" # Saving $75/mo
+    "i-0162667a" = "t3.large" # Saving $75/mo
+    "i-0120212a" = "t3.large" # Saving $75/mo
+    "i-0274645a" = "t3.large" # Saving $75/mo
+    "i-0450314a" = "t3.large" # Saving $75/mo
+    "i-0758686a" = "t3.large" # Saving $75/mo
+    "i-0483488a" = "t3.large" # Saving $75/mo
+    "i-0703502a" = "t3.large" # Saving $75/mo
+    "i-0469713a" = "t3.large" # Saving $75/mo
+    "i-0792519a" = "t3.large" # Saving $75/mo
+    "i-0439615a" = "t3.large" # Saving $75/mo
+    "i-0420796a" = "t3.large" # Saving $75/mo
+    "i-0527842a" = "t3.large" # Saving $75/mo
+    "i-0851987a" = "t3.large" # Saving $75/mo
+    "i-0690341a" = "t3.large" # Saving $75/mo
+    "i-0792072a" = "t3.large" # Saving $75/mo
+    "i-0220093a" = "t3.large" # Saving $75/mo
+    "i-0969272a" = "t3.large" # Saving $75/mo
+    "i-0809588a" = "t3.large" # Saving $75/mo
+  }
+}
